7de9c18d80db8ab6f66c2d6da0bc4de7c6142416
Sam! Bonfante <sam.bonfante@opentrons.com>
2021-10-05 13:35:09 -0400

    remove CDNs for build consistency

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import Vue, { createApp } from "vue";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "flag-icon-css/css/flag-icon.min.css";
+import "simplebar";
+import "simplebar/dist/simplebar.css";
 
 import "./assets/styles.css";
 
@@ -21,4 +26,5 @@ const app = createApp(App)
   .use(i18n)
   .use(VueGtag, { config: { id: "G-7MZR4NX0GD" } });
 
+app.config.performance = true;
 app.mount("#app");
